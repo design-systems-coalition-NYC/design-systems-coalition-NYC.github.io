3556108ef782f9d51c637de1e59038a7a7f9a993
Brent Jackson <jxnblk@gmail.com>
2017-09-26 13:19:28 -0400

    Prototype new Lab/Zero setup

---
+++ home.jsx
@@ -0,0 +1,43 @@
+---
+title: Design Systems Coalition NYC
+description: A community for people who build design systems to share and learn from each other.
+---
+<Box>
+  <BannerHeader
+    heading="Design Systems Coalition"
+    subhead="NYC"
+    text="A community for people who build design systems to share and learn from each other."
+  />
+</Box>
+<Box>
+  <Section
+    button="RSVP"
+    heading="Defining design principles"
+    href="#!"
+    subhead="Oct 4 @ The New York Times"
+  />
+</Box>
+<Box>
+  <Container px={4} pt={2} pb={5}>
+    <Heading fontSize={4} m={0} mb={3}>
+      Speakers
+    </Heading>
+    <Grid width={[1, 0.3333333333333333]}>
+      <Card
+        heading="Orr Shtuhl"
+        image="http://designsystems.nyc/assets/orrshtuhl.jpg"
+        text="Orr is senior product designer at Wirecutter, where he helps bring trust to reviews on the internet. Previously, he was director of UX at the interactive agency Blenderbox. He has written for A List Apart, and appeared at SXSW and in Fast Company. Orr is also a beer and cheese instructor, and gives beer tours as The Bestest. He has opinions about yeast."
+      />
+      <Card
+        heading="Michael Jovel"
+        image="http://designsystems.nyc/assets/michael.jpg"
+        text="Michael is a Front-end Developer at the U.S. Food and Drug Administration. He is also the past organizer Bmoresponsive, a conference dedicated to creating things for the multi-device web. Prior to joining the FDA Michael worked for the Department of Defense."
+      />
+      <Card
+        heading="Caitlin Osbahr"
+        image="http://designsystems.nyc/assets/caitlin.jpg"
+        text="Caitlin is a senior product designer at BuzzFeed. She's currently helping build cross-platform digital products for Tasty, including the recently-launched Tasty app for iOS."
+      />
+    </Grid>
+  </Container>
+</Box>
