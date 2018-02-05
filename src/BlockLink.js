4892922fa14bd4cb66b75b790538bfa2c7778c8f
Brent Jackson <jxnblk@gmail.com>
2018-02-04 19:15:39 -0500

    Add event and speaker section components

---
+++ src/BlockLink.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const BlockLink = styled.a`
+  display: block;
+  text-decoration: none;
+  color: inherit;
+`
+
+export default BlockLink
--- src/Index.js
+++ src/Index.js
@@ -4,17 +4,26 @@ import Hero from './Hero'
 import Container from './Container'
 import Box from './Box'
 import Heading from './Heading'
+import EventSection from './EventSection'
+import SpeakerSection from './SpeakerSection'
 import PastEventCard from './PastEventCard'
 import HostPromo from './HostPromo'
 import Organizers from './Organizers'
 import Footer from './Footer'
 import { pastEvents } from './content'
-console.log(pastEvents)
 
 // Homepage index.html
 const Index = props => (
   <Page>
     <Hero />
+    <EventSection
+      title='Defining design principles'
+      date='Oct 4'
+      location='The New York Times'
+      href='https://www.meetup.com/NYC-Design-Systems-Coalition/events/242645527/'
+      button='See who went'
+    />
+    <SpeakerSection />
     <section id='past-meetups'>
       <Container py={5}>
         <Heading fontSize={[ 5, 6 ]}>
