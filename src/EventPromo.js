9ce05b443b65963f240e1e2b1aca6c265143a20e
Diana Mounter <broccolini@github.com>
2018-02-06 11:17:16 -0500

    update homepage with happy hour and save the date

---
+++ src/EventPromo.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Container from './Container'
+import Heading from './Heading'
+import Caps from './Caps'
+import Button from './Button'
+import Image from './Image'
+import Flex from './Flex'
+import Box from './Box'
+import Text from './Text'
+
+const EventPromo = (props) => (
+  <Container py={6}>
+    <Flex wrap align='center'>
+      <Box py={4} width={[ 1, 2/3, 1/2 ]}>
+        <Heading
+          mb={3}
+          fontSize={[ 5, 6]}>
+          {props.title}
+        </Heading>
+        <Caps mb={3}>{props.date} @ {props.location}</Caps>
+        <Text mb={3} pr={[0,4]}>{props.text}</Text>
+        <Button.a href={props.href}>
+          {props.button}
+        </Button.a>
+      </Box>
+      <Image py={4} src={props.image} width={[ 1, 1/3, 1/2 ]} />
+    </Flex>
+  </Container>
+)
+
+export default EventPromo
--- src/Index.js
+++ src/Index.js
@@ -4,7 +4,9 @@ import Hero from './Hero'
 import Container from './Container'
 import Box from './Box'
 import Heading from './Heading'
+import SaveDate from './SaveDate'
 import EventSection from './EventSection'
+import EventPromo from './EventPromo'
 import SpeakerSection from './SpeakerSection'
 import PastEventCard from './PastEventCard'
 import HostPromo from './HostPromo'
@@ -16,14 +18,23 @@ import { pastEvents } from './content'
 const Index = props => (
   <Page>
     <Hero />
-    <EventSection
-      title='Defining design principles'
-      date='Oct 4'
-      location='The New York Times'
-      href='https://www.meetup.com/NYC-Design-Systems-Coalition/events/242645527/'
-      button='See who went'
+    <EventPromo
+      title='February Happy Hour!'
+      date='Feb 7'
+      location='The Winslow'
+      href='https://www.meetup.com/NYC-Design-Systems-Coalition/events/247261426/'
+      button='RSVP'
+      text='Join us for drinks and conversation, and make new connections in the design systems community.'
+      image='https://user-images.githubusercontent.com/334891/35786610-685e924e-09f6-11e8-846e-f57839488288.jpg'
     />
-    <SpeakerSection />
+    {/*
+      <SpeakerSection />
+    */}
+    <SaveDate
+      href='https://www.meetup.com/NYC-Design-Systems-Coalition/events/247534331/'
+      title='Save the date!'
+      desc='March 8'
+    />
     <section id='past-meetups'>
       <Container py={5}>
         <Heading fontSize={[ 5, 6 ]}>
