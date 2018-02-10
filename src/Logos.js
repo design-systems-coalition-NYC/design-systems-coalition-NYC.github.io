59fc3a11f9c0aff56550949ace56c6c2b66595b7
Diana Mounter <broccolini@github.com>
2018-02-09 19:06:48 -0500

    add logos and innerheader to styleguide page

---
+++ src/Logos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Box from './Box'
+
+const Logos = (props) => (
+  <Box width={[1, 2/3, 1/2]}>
+    <iframe style={{ borderWidth: '4px', borderStyle: 'solid', borderColor: '#eee', width:'800px', height:'450px'}} src='https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/file/QaYbQ2LGdA3lefOWZCosDcrt/DSC-NYC-logos' allowfullscreen></iframe>
+  </Box>
+)
+
+export default Logos
