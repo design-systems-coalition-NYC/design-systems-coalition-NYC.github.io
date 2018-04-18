28fd58551baa8236c1784cbe6aef1ec30b6f2a78
Diana Mounter <broccolini@github.com>
2018-04-18 13:53:30 -0400

    fix url for rsvps

--- src/Index.js
+++ src/Index.js
@@ -23,7 +23,7 @@ const Index = props => (
       date='March 8'
       location='ustwo'
       image='https://user-images.githubusercontent.com/30531428/28724847-72a707e4-7389-11e7-94d2-950944841032.jpg'
-      href='https://www.meetup.com/NYC-Design-Systems-Coalition/events/247534331/'
+      href='https://www.meetup.com/NYC-Design-Systems-Coalition/events/247693556/'
       text='Design systems help provide a unified language but the path to getting there can be challenging. Successful adoption is often due to the collaboration of a multi-disciplinary team. Our speakers will be sharing how they bridge the gap between design, development, content writers, and other stakeholders.'
       button='RSVP'
     />
