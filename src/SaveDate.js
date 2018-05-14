777c6e79ff1e6fd3c436a5376c142535d1602b5b
Diana Mounter <broccolini@github.com>
2018-05-14 10:51:45 -0400

    layout tweaks: - move saveDate and update styles - move speakerSection and tweak padding - update order and homepage content

--- src/SaveDate.js
+++ src/SaveDate.js
@@ -3,23 +3,24 @@ import Box from './Box'
 import Container from './Container'
 import Flex from './Flex'
 import Text from './Text'
-import NavLink from './NavLink'
+import Link from './NavLink'
 
 const SaveDate = (props) => (
-    <Box color='white' bg='black' my={5}>
+    <Box color='white' bg='black' mb={4}>
       <Container>
         <Flex wrap
           align='baseline'
           mx={-2}
           py={4}>
-          <NavLink
+          <Link
             fontSize={5}
             m={2}
+            color='tomato'
             href={props.href}>
-            Save the date!
-          </NavLink>
-          <Text fontWeight={400} m={2} pl={[0,1]} fontSize={5}>Upcoming meetup&#58;</Text>
-          <Text fontWeight={400} m={2} fontSize={5}>{props.desc}</Text>
+            Next meetup&#58;
+            <Text.span fontWeight={400} m={2} pl={[0,1]} fontSize={5}>Happy Hour!</Text.span>
+            <Text.span fontWeight={400} m={2} fontSize={5}>{props.desc}</Text.span>
+          </Link>
         </Flex>
       </Container>
     </Box>
