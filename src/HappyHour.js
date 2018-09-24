1da3375385cc4014821d87ad210b19c36577fdcc
Diana Mounter <broccolini@github.com>
2018-09-23 21:36:32 -0400

    make a happy hour component

---
+++ src/HappyHour.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Container from './Container'
+import Heading from './Heading'
+import Caps from './Caps'
+import Button from './Button'
+import Image from './Image'
+import Flex from './Flex'
+import Box from './Box'
+import Text from './Text'
+import Link from './Link'
+
+const HappyHour = (props) => (
+  <Container id='next-meetup' py={6}>
+    <Flex flexWrap='wrap' alignItems='center'>
+      <Box py={4} width={[1, 2/3]}>
+        <Heading
+          mb={3}
+          fontSize={[ 5, 6]}>
+          {props.title}
+        </Heading>
+        <Caps mb={3}>{props.date} @ {props.location}</Caps>
+        <Text mb={3} pr={[0,4]}>{props.text}</Text>
+        <Button.a href={props.href}>
+          {props.button}
+        </Button.a>
+      </Box>
+      <Box width={[ 1/2, 1/3, 1/4]}>
+        <Link href={props.sponsorlink}><Image py={4} src={props.image}/></Link>
+      </Box>
+    </Flex>
+  </Container>
+)
+
+export default HappyHour
